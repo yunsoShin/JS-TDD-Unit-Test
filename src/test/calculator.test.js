c0a48e1d177c6eaaade46953a04c5a07abd0c4e7
yunsoo1 <shin4895@gmail.com>
2022-07-03 20:33:13 +0900

    Update calculator.test.js

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -14,8 +14,36 @@ test('Cal', () => {
     expect(TestCalculator.value).toBe(6);
     TestCalculator.divide(6)
     expect(TestCalculator.value).toBe(1);
-
-    
-
-
 });
+
+describe('CAL',()=>{
+    let cal 
+    beforeEach(()=>{cal = new Calculator;})
+    test('set',()=>{
+        cal.set(2)
+        expect(cal.value).toBe(2);
+    })
+    test('add',()=>{
+        cal.add(1)
+        expect(cal.value).toBe(1)
+    })
+    test('subtract',()=>{
+        cal.add(50)
+        cal.subtract(20)
+        expect(cal.value).toBe(30)
+    })
+    it('Divide',()=>{
+        cal.set(20)
+        cal.divide(2)
+        expect(cal.value).toBe(10)
+    })
+
+    describe('Expressioin',()=>{
+        it('0/0',()=>{
+            cal.divide(0)
+            expect(cal.value).toBe(0);
+        })
+    })
+
+})
+
