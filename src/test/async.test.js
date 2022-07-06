ecef52dc0741a134fb164ccae1bf57646fde7ede
yunsoo1 <shin4895@gmail.com>
2022-07-06 21:30:37 +0900

    Update async.test.js

--- src/test/async.test.js
+++ src/test/async.test.js
@@ -3,7 +3,7 @@ const fetchs = require('../async.js');
 describe('Async',()=>{
     it('',()=>{
         fetchs().then((item)=>{
-            expect(item).toEqual({item :'hanho' , price:1390});
+            expect(item).toEqual({item :'ha' , price:1390});
 
         });
     });
