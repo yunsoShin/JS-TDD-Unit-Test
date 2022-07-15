804ba62e6c9837776ceab9e13c74e1934cc80267
yunsoo1 <shin4895@gmail.com>
2022-07-15 20:30:29 +0900

    Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -2,15 +2,12 @@ class Calculator {
   constructor() {
     this.value = 0;
   }
-
   set(num) {
     this.value = num;
   }
-
   clear() {
     this.value = 0;
   }
-
   add(num) {
     const sum = this.value + num;
     if (sum > 100) {
@@ -18,15 +15,12 @@ class Calculator {
     }
     this.value = sum;
   }
-
   subtract(num) {
     this.value = this.value - num;
   }
-
   multiply(num) {
     this.value = this.value * num;
   }
-
   divide(num) {
     this.value = this.value / num;
   }
