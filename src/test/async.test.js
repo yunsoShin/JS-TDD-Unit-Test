fed9b2aec6aeed8a4c115c7fed2c302f0a61457e
yunsoo1 <shin4895@gmail.com>
2022-07-28 03:00:20 +0900

    Update async.test.js

--- src/test/async.test.js
+++ src/test/async.test.js
@@ -16,7 +16,7 @@ describe('Async', () => {
 
   it('async - await', async () => {
     const product = await fetchProduct();
-    expect(product).toEqual({ item: 'Milk', price: 200 });
+    expect(product).toEqual({ item: 'Mil', price: 200 });
   });
 
   it('async - resolves', () => {
