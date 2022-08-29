fd98fbf0bd81ef67bb67ee7e8feb0c54157317a1
yunsoo1 <shin4895@gmail.com>
2022-08-29 22:48:34 +0900

    Update async.js

--- src/async.js
+++ src/async.js
@@ -2,7 +2,7 @@ function fetchProduct(error) {
   if (error === 'error') {
     return Promise.reject('network error');
   }
-  return Promise.resolve({ item: 'Mil', price: 200 });
+  return Promise.resolve({ item: 'M', price: 200 });
 }
 
 module.exports = fetchProduct;
